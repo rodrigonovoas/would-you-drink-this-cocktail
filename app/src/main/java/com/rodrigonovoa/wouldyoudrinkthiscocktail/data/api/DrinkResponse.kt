7781d8d7f2358621839553db26d74b7a5c62b390
rodrigonovoasalgado <rodrigonovoa97s@gmail.com>
2024-04-06 21:50:04 +0200

    adding MainActivityViewModel Test

--- app/src/main/java/com/rodrigonovoa/wouldyoudrinkthiscocktail/data/api/DrinkResponse.kt
+++ app/src/main/java/com/rodrigonovoa/wouldyoudrinkthiscocktail/data/api/DrinkResponse.kt
@@ -9,31 +9,10 @@ data class DrinksResponse(
 data class DrinkResponse(
     val idDrink: String,
     val strDrink: String,
-    val strDrinkAlternate: String?,
-    val strTags: String?,
-    val strVideo: String?,
     val strCategory: String,
-    val strIBA: String?,
     val strAlcoholic: String,
-    val strGlass: String,
     val strInstructions: String,
-    val strInstructionsES: String?,
-    val strInstructionsDE: String?,
-    val strInstructionsFR: String?,
-    val strInstructionsIT: String?,
-    val strInstructionsZH_HANS: String?,
-    val strInstructionsZH_HANT: String?,
-    val strDrinkThumb: String,
-    val strIngredient1: String?,
-    val strIngredient2: String?,
-    val strIngredient3: String?,
-    val strMeasure1: String?,
-    val strMeasure2: String?,
-    val strMeasure3: String?,
-    val strImageSource: String?,
-    val strImageAttribution: String?,
-    val strCreativeCommonsConfirmed: String,
-    val dateModified: String
+    val strDrinkThumb: String
 ) {
     fun fromResponseToDrink(): Drink {
         return Drink(
---
+++ app/src/test/java/com/rodrigonovoa/wouldyoudrinkthiscocktail/MainActivityViewModelTest.kt
@@ -0,0 +1,101 @@
+package com.rodrigonovoa.wouldyoudrinkthiscocktail
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.rodrigonovoa.wouldyoudrinkthiscocktail.data.api.DrinkResponse
+import com.rodrigonovoa.wouldyoudrinkthiscocktail.data.api.DrinksResponse
+import com.rodrigonovoa.wouldyoudrinkthiscocktail.data.db.Drink
+import com.rodrigonovoa.wouldyoudrinkthiscocktail.repository.ApiResult
+import com.rodrigonovoa.wouldyoudrinkthiscocktail.ui.mainActivity.MainActivityViewModel
+import com.rodrigonovoa.wouldyoudrinkthiscocktail.useCase.GetCocktailUseCase
+import com.rodrigonovoa.wouldyoudrinkthiscocktail.useCase.InsertDrinkUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.*
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mockito.Mockito
+
+
+@ExperimentalCoroutinesApi
+@RunWith(JUnit4::class)
+class MainActivityViewModelTest {
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var getCocktailUseCase: GetCocktailUseCase
+    private lateinit var insertDrinkUseCase: InsertDrinkUseCase
+    private lateinit var mainActivityViewModel: MainActivityViewModel
+
+    @Before
+    fun setUp() {
+        getCocktailUseCase = Mockito.mock()
+        insertDrinkUseCase = Mockito.mock()
+        mainActivityViewModel = MainActivityViewModel(getCocktailUseCase, insertDrinkUseCase)
+
+        Dispatchers.setMain(TestCoroutineDispatcher())
+    }
+
+
+    @Test
+    fun `getDrinkFromAPI returns drink object successful`() = runTest {
+        val mockDrinksResponse = DrinksResponse(listOf())
+        Mockito.`when`(getCocktailUseCase.invoke()).thenReturn(
+            flowOf(
+                ApiResult.loading(),
+                ApiResult.success(mockDrinksResponse)
+            )
+        )
+
+        // When & Then
+        mainActivityViewModel.getDrinkFromAPI()
+
+        // Assert
+        Assert.assertTrue(mainActivityViewModel.drink.first() != null)
+        Assert.assertTrue(mainActivityViewModel.isLoading.value == false)
+    }
+
+    @Test
+    fun `insertDrink with filled DrinkResponse inserts drink successful`() = runTest {
+        val mockDrinkResponse = DrinkResponse("", "", "", "",
+        "", "")
+        val mockDrink = Drink("", "", "", "", "")
+
+        Mockito.`when`(insertDrinkUseCase.invoke(mockDrink)).thenReturn(flowOf(true))
+
+        // When & Then
+        mainActivityViewModel.insertDrink(mockDrinkResponse)
+
+        // Assert
+        Assert.assertTrue(mainActivityViewModel.drinkInserted.first() == true)
+    }
+
+    @Test
+    fun `insertDrink with NULL DrinkResponse does not insert`() = runTest {
+        // When & Then
+        mainActivityViewModel.insertDrink(null)
+
+        // Assert
+        Assert.assertTrue(mainActivityViewModel.drinkInserted.first() == false)
+    }
+
+}
+
+@ExperimentalCoroutinesApi
+class MainDispatcherRule(val dispatcher: TestDispatcher = StandardTestDispatcher()): TestWatcher() {
+
+    override fun starting(description: Description?) = Dispatchers.setMain(dispatcher)
+
+    override fun finished(description: Description?) = Dispatchers.resetMain()
+
+}
